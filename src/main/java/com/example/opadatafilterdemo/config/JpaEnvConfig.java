08d10935d7a4c8bff375b56e819badad2f7c48e8
Joffry Ferrater <joffry.ferrater@bisnode.com>
2020-04-22 16:38:39 +0200

    Removed EntityScan annotation

--- src/main/java/com/example/opadatafilterdemo/config/JpaEnvConfig.java
+++ src/main/java/com/example/opadatafilterdemo/config/JpaEnvConfig.java
@@ -1,16 +1,14 @@
 package com.example.opadatafilterdemo.config;
 
 import com.github.jferrater.opa.data.filter.spring.boot.starter.repository.jpa.OpaRepositoryFactoryBean;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaRepositories(
-        value = "com.example.opadatafilterdemo.repository"
-        ,repositoryFactoryBeanClass = OpaRepositoryFactoryBean.class
+        value = "com.example.opadatafilterdemo.repository",
+        repositoryFactoryBeanClass = OpaRepositoryFactoryBean.class
 )
-@EntityScan("com.example.opadatafilterdemo.repository")
 public class JpaEnvConfig {
 
 }
